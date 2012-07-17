56e1b720fa94297613d2b4ec29f220eeb940b126
Honza Pokorny <me@honza.ca>
2012-07-17 07:29:06 -0300

    Add vim, openssl and fix postgres version

--- deploy/cookbooks/djangoapp/recipes/default.rb
+++ deploy/cookbooks/djangoapp/recipes/default.rb
@@ -2,16 +2,19 @@ execute "Update apt repos" do
     command "apt-get update"
 end
 
+package 'vim'
+
 include_recipe 'nginx'
 include_recipe 'build-essential'
 include_recipe 'python'
+include_recipe 'openssl'
 include_recipe 'postgresql::server'
 include_recipe 'memcached'
 include_recipe 'runit'
 include_recipe 'git'
 
 execute "restart postgres" do
-    command "sudo /etc/init.d/postgresql-8.4 restart"
+    command "sudo /etc/init.d/postgresql restart"
 end
 
 execute "create-database" do
