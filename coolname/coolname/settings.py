9e71b969151950ddde5020c4d0baee7c8c51c503
Honza Pokorny <me@honza.ca>
2013-04-04 09:49:14 -0300

    Install django admin

--- coolname/coolname/settings.py
+++ coolname/coolname/settings.py
@@ -115,10 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
 )
 
 # A sample logging configuration. The only tangible logging
