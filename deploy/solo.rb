277480dbf7cb87fe6b6a5046e68aedc67fdf4697
Honza Pokorny <me@honza.ca>
2013-04-04 10:18:32 -0300

    Remove things

--- deploy/solo.rb
+++
@@ -1,8 +0,0 @@
-#log_level :info
-#cookbook_path "/etc/chef/cookbooks"
-#json_attribs "/etc/chef/node.json"
-#role_path "/etc/chef/roles"
-root = File.absolute_path(File.dirname(__FILE__))
- 
-file_cache_path root
-cookbook_path root + '/cookbooks'
--- gunicorn.py
+++
@@ -1,3 +0,0 @@
-bind = '127.0.0.1:8888'
-workers = 2
-pidfile = 'gunicorn.pid'
